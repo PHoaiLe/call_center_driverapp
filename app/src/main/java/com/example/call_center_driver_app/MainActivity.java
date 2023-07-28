c04520e68bee23b42b7ca04d17fd4819c06a8398
PHoaiLe <lehoaiphuong13042002@gmail.com>
2023-07-28 23:59:55 +0700

    feat: main activity and login, sign up activity initialization

--- app/src/main/java/com/example/call_center_driver_app/MainActivity.java
+++ app/src/main/java/com/example/call_center_driver_app/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.call_center_driver_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends FragmentActivity {
+
+    private FragmentTransaction fragmentTransaction;
+    private BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.main_bottom_nav);
+
+
     }
 }
